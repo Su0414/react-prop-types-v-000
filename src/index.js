3dd4f01b994f0c1fe2a7b4c3ddc8d92b78c85fb5
Suchitra Naik <susanaik@gmail.com>
2018-10-08 19:00:21 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Order extends React.Component {
- 
+
   render() {
     return (
       <div className="order">
@@ -22,5 +22,13 @@ Order.defaultProps = {
   cone: true,
   size: 'regular'
 };
- 
-Order.propTypes = {};
+
+Order.propTypes = {
+  cone: PropTypes.bool,
+  size: PropTypes.string,
+  scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired // We're using UNIX timestamps here
+  }).isRequired
+};
